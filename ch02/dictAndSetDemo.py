cc623c38094c0f67803d0721a98324663d61428f
Bowen LU <bowenlu.cloud@gmail.com>
2022-10-19 03:43:45 +0000

    full ch02

---
+++ ch02/dictAndSetDemo.py
@@ -0,0 +1,20 @@
+d = {'zhangsan': 65, 'lisi': 80, 'wangwu': 100}
+for key in d:
+    print(d[key])
+    
+    
+s = set([1, 1, 2, 3, 4, 5, 6])
+print(s)
+s.add(4)
+print(s)
+s.remove(5)
+print(s)
+s.add(100)
+print(s)
+
+a = 'abc'
+print(a)
+print(a.replace('a', 'S'))
+print(a)
+b = a.replace('a', 'C')
+print(b)
