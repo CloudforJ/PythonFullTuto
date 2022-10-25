9be7e75b6b6efd70270496df5e21e4cd16897ecc
CloudforJ <92019313+CloudforJ@users.noreply.github.com>
2022-10-25 22:51:43 +0800

    decorator demo

---
+++ ch05/decoratorDemo.py
@@ -0,0 +1,18 @@
+import functools
+
+def log(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kw):
+        print('call %s():' % func.__name__)
+        return func(*args, **kw)
+    return wrapper
+
+
+@log
+def now():
+    print('2022-10-24')
+
+f = now
+f()
+print(now.__name__)
+print(f.__name__)
